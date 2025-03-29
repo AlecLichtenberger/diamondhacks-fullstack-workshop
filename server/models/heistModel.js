4aaaf7a049e9483e3a5850ca3c5f852265697716
Alec Lichtenberger <lichtenbergeralec@gmail.com>
2025-03-28 18:16:32 -0700

    Added some changes to the mongoDB

--- server/models/heistModel.js
+++ server/models/heistModel.js
@@ -4,7 +4,10 @@ const mongoose = require('mongoose');
 // TODO: Name each of the fields for the heist and their types
   // HINT: Look at server/models/crewModel.js if you're stuck!
 const Heistschema = new mongoose.Schema({
-  // ...
+  location: {type: String},
+  startDate: {type: date},
+  endDate: {type: date},
+  planEntry: {type: String}
 });
 
 // Create a model for our Heists based on the schema
